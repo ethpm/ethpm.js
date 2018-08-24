8157f3f1da183eb7adeb0ab5d17f271901a8edc8
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-08-24 19:06:33 -0400

    Add initial implementation to query packages

---
+++ src/workspace/query.ts
@@ -0,0 +1,62 @@
+import { Maybe } from "types";
+import { PackageQuery } from "ethpm/package/query";
+import * as manifest from "ethpm/manifest";
+import * as pkg from "ethpm/package/package";
+import * as storage from "ethpm/storage";
+
+export interface Options {
+  package: pkg.Package;
+  resolver: storage.Resolver;
+  read: manifest.Reader;
+}
+
+export class WorkspaceQuery implements PackageQuery {
+  package: pkg.Package;
+
+  private resolver: storage.Resolver;
+  private read: manifest.Reader;
+
+  constructor (options: Options) {
+    this.package = options.package;
+    this.resolver = options.resolver;
+    this.read = options.read;
+  }
+
+  async contractType(ref: pkg.ContractTypeReference)
+    : Promise<pkg.ContractType>
+  {
+    return this.package.contractTypes[ref];
+  }
+
+  async contractInstance(
+    chain: pkg.ChainURI,
+    name: pkg.ContractInstanceName
+  )
+    : Promise<pkg.ContractInstance>
+  {
+    const deployment = this.package.deployments.get(chain)
+    if (deployment === undefined) {
+      throw new Error(`Could not find deployment at chain ${chain.href}`);
+    }
+
+    const instance = deployment[name];
+    if (instance === undefined) {
+      throw new Error(`Could not find instance "${name}" on chain ${chain.href}`);
+    }
+
+    return instance;
+  }
+
+  async buildDependency(name: pkg.PackageName)
+    : Promise<pkg.Package>
+  {
+    const uri = this.package.buildDependencies[name];
+    const contents = await this.resolver.get(uri);
+    if (contents !== undefined) {
+      return this.read(contents);
+    }
+
+    throw new Error(`Could not find build dependency "${name}"`);
+  }
+
+}
---
+++ src/workspace/test/query.ts
@@ -0,0 +1,51 @@
+import { examplesResolver } from "test/stub/ipfs";
+import examples from "test/examples/packages";
+
+import { WorkspaceQuery } from "ethpm/workspace/query";
+import v2 from "ethpm/manifest/v2";
+
+
+it("resolves package-level contract types", async () => {
+  let query = new WorkspaceQuery({
+    package: examples["standard-token"],
+    resolver: examplesResolver,
+    read: v2.read,
+  });
+
+  const standardToken = await query.contractType("StandardToken");
+
+  expect(standardToken).toEqual(
+    examples["standard-token"].contractTypes["StandardToken"]
+  );
+});
+
+it("resolves deployed instances", async () => {
+  let query = new WorkspaceQuery({
+    package: examples["piper-coin"],
+    resolver: examplesResolver,
+    read: v2.read,
+  });
+
+  const [chain] = examples["piper-coin"].deployments.keys()
+  const deployment = examples["piper-coin"].deployments.get(chain);
+
+  const expectedInstance = deployment["PiperCoin"];
+
+  const piperCoin = await query.contractInstance(chain, "PiperCoin");
+
+  expect(piperCoin).toEqual(expectedInstance);
+
+});
+
+it("resolves build dependencies", async () => {
+  let query = new WorkspaceQuery({
+    package: examples["piper-coin"],
+    resolver: examplesResolver,
+    read: v2.read,
+  });
+
+  const standardToken = await query.buildDependency("standard-token");
+
+  expect(standardToken).toEqual(examples["standard-token"]);
+});
+
