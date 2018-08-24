28b22c1dc8e06214389fc2c0da572a0fd3404548
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-08-24 19:05:17 -0400

    Add pre-filled resolver stub for examples

--- test/stub/ipfs.ts
+++ test/stub/ipfs.ts
@@ -1,6 +1,8 @@
-import getHash from "ethpm/storage/ipfs/hash";
+import { URL } from "url";
 
 import { Maybe } from "types";
+import getHash from "ethpm/storage/ipfs/hash";
+import examples from "test/examples/manifests";
 
 export class Resolver {
   private contents: Record<string, string>;
@@ -14,7 +16,34 @@ export class Resolver {
     this.contents[`ipfs://${hash}`] = content;
   }
 
-  async get (uri: string): Promise<Maybe<string>> {
+  async get (uri: string | URL): Promise<Maybe<string>> {
+    if (uri instanceof URL) {
+      uri = uri.href;
+    }
     return this.contents[uri];
   }
 }
+
+export class ExamplesResolver extends Resolver {
+  private ready: Promise<any>;
+
+  constructor () {
+    super();
+
+    this.ready = Promise.all(
+      Object.values(examples).map(async (manifest) => {
+        await this.add(manifest)
+      })
+    );
+  }
+
+  async get (uri: any): Promise<Maybe<string>> {
+    await this.ready;
+
+    return await super.get(uri);
+  }
+}
+
+const examplesResolver = new ExamplesResolver();
+
+export { examplesResolver };
--- test/stub/test/ipfs.ts
+++ test/stub/test/ipfs.ts
@@ -1,28 +1,39 @@
 import hash from "ethpm/storage/ipfs/hash";
 
-import { Resolver as StubResolver } from "test/stub/ipfs";
+import { Resolver as StubResolver, examplesResolver } from "test/stub/ipfs";
+import examples from "test/examples/manifests";
 
-it("records and retrieves string values by hash", async () => {
-  const resolver = new StubResolver()
+describe("StubResolver", () => {
+  it("records and retrieves string values by hash", async () => {
+    const resolver = new StubResolver()
 
-  const contents = [
-    'hello world',
-    '{"manifest_version":"2","package_name":"owned","version":"1.0.0"}',
-    'readme'
-  ];
+    const contents = [
+      'hello world',
+      '{"manifest_version":"2","package_name":"owned","version":"1.0.0"}',
+      'readme'
+    ];
 
-  // setup
-  for (let content of contents) {
-    resolver.add(content);
-  }
+    // setup
+    for (let content of contents) {
+      resolver.add(content);
+    }
 
-  const hashes = await Promise.all(contents.map(hash))
-  const expectedUris = hashes.map(result => `ipfs://${result}`);
+    const hashes = await Promise.all(contents.map(hash))
+    const expectedUris = hashes.map(result => `ipfs://${result}`);
 
-  // test URI lookup
-  for (let [idx, uri] of expectedUris.entries()) {
-    const retrieved = await resolver.get(uri);
+    // test URI lookup
+    for (let [idx, uri] of expectedUris.entries()) {
+      const retrieved = await resolver.get(uri);
 
-    expect(retrieved).toEqual(contents[idx]);
-  }
+      expect(retrieved).toEqual(contents[idx]);
+    }
+  });
+});
+
+it("retrives examples", async () => {
+  const owned = examples["owned"];
+  const hashed = await hash(owned);
+  const uri = `ipfs://${hashed}`;
+
+  expect(await examplesResolver.get(uri)).toEqual(owned);
 });
