3819f8dbd8e493c70c963383c2b472cdd2e99398
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-08-24 22:51:22 -0400

    Add type for test example packages

--- test/examples/packages.ts
+++ test/examples/packages.ts
@@ -1,7 +1,12 @@
+import * as pkg from "ethpm/package/package";
 import manifests from "./manifests";
 import v2 from "ethpm/manifest/v2";
 
-const packages = Object.assign(
+interface PackageMap {
+  [name: string]: pkg.Package
+}
+
+const packages: PackageMap = Object.assign(
   {}, ...Object.entries(manifests).map(
     ([ name, manifest ]) => ({
       [name]: v2.read(manifest)
