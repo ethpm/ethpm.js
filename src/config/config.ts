7b548c1e2b74d831a28d5cbd014faa8f4a7a0079
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-09-10 22:26:47 -0400

    Define abstract Connector<S> class

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,9 @@
  * @module "ethpm/config"
  */
 
+import * as t from "io-ts";
+import { ThrowReporter } from "io-ts/lib/ThrowReporter";
+
 export type ConfigValue = string;
 
 export type HasManifest = { manifest: any };
@@ -18,3 +21,18 @@ export type Config =
 export type RawConfig<T extends Config> = {
   [K in keyof T]: ConfigValue
 } & { [k: string]: ConfigValue };
+
+export abstract class Connector<S> {
+  abstract optionsType: any;
+
+  abstract async init (...args: any[]): Promise<S>;
+
+  async connect (options: t.mixed): Promise<S> {
+    const validation = this.optionsType.decode(options);
+    if (!validation.isRight()) {
+      ThrowReporter.report(validation);
+    }
+
+    return this.init(validation.value);
+  }
+}
