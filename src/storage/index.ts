da6531ebd5f20e3ea06674f6a88574f316d374aa
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-09-10 22:30:08 -0400

    Inline storage interface function types

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -5,12 +5,8 @@
 import { Maybe } from "types";
 import { URL } from "url";
 
-export type Read = (uri: URL) => Promise<Maybe<string>>;
-export type Hash = (content: string) => Promise<string>;
-export type PredictUri = (content: string) => Promise<URL>;
-
 export interface Service {
-  read: Read;
-  hash: Hash;
-  predictUri: PredictUri;
+  read (uri: URL): Promise<Maybe<string>>;
+  hash (content: string): Promise<string>;
+  predictUri (content: string): Promise<URL>;
 }
