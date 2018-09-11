3924113ac852c72f5475aeb770e8aa13e81a25cd
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-09-10 20:03:36 -0400

    Inline function types for manifest service

--- src/manifest/types.ts
+++ src/manifest/types.ts
@@ -4,16 +4,7 @@
 
 import { Package } from "ethpm/package";
 
-export type Read = (json: string) => Promise<Package>;
-export type Write = (pkg: Package) => Promise<string>;
-
-export interface ManifestVersion {
-  version: string;
-  read: Read;
-  write: Write;
-}
-
 export interface Service {
-  read: Read;
-  write: Write;
+  read (json: string): Promise<Package>;
+  write (pkg: Package): Promise<string>;
 }
