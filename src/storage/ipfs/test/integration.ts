86fd2a89d85f538d2c0a00941a7e3fe58fc87f3c
Adrian Li <li.adrianmc@gmail.com>
2018-10-31 14:16:26 -0400

    clarify test

--- src/storage/ipfs/test/integration.ts
+++ src/storage/ipfs/test/integration.ts
@@ -4,20 +4,24 @@ const IPFSFactory = require("ipfsd-ctl");
 
 describe("IPFS integration", () => {
   let daemon: any;
+  let host: string;
+  let port: string;
 
   const startDaemon = () =>
     new Promise(resolve => {
       const f = IPFSFactory.create({ type: "js" });
       f.spawn((err: any, ipfsd: any) => {
-        daemon = ipfsd
-        resolve()
+        daemon = ipfsd;
+        host = daemon.api.apiHost;
+        port = daemon.api.apiPort;
+        resolve();
       });
     });
 
   beforeAll(() => startDaemon(), 20000);
   afterAll(() => daemon.stop());
 
-  it("should show the IPFS daemon to be initialized", () => {
+  it("should initialize the IPFS daemon properly", () => {
     expect(daemon.initialized).toBe(true);
   });
 });
