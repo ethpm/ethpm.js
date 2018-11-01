a9884d61ea846e5d9bcdfbde85e977b692ea7b3f
Mike Seese <seesemichaelj@gmail.com>
2018-11-01 05:30:11 +0100

    reorganize the service base interface

--- src/registries/service.ts
+++ src/registries/service.ts
@@ -5,28 +5,32 @@
 import { URL } from "url";
 
 import * as pkg from "ethpm/package";
+import PackagesCursor from "./web3/cursors/packages";
+import ReleasesCursor from "./web3/cursors/releases";
+
+export type PackageCursor = {
+  /**
+   * List all releases
+   */
+  releases (): ReleasesCursor;
+
+
+  /**
+   * Retrieve manifest URI for specific version
+   */
+  release (version: pkg.Version): Promise<URL>;
+};
 
 export interface Service {
   /**
    * List all packages
    */
-  packages (): IterableIterator<Promise<pkg.PackageName>>;
+  packages (): Promise<PackagesCursor>;
 
   /**
    * Query a particular package
    */
-  package (packageName: pkg.PackageName): {
-    /**
-     * List all releases
-     */
-    releases (): IterableIterator<Promise<pkg.Version>>;
-
-
-    /**
-     * Retrieve manifest URI for specific version
-     */
-    release (version: pkg.Version): Promise<URL>;
-  };
+  package (packageName: pkg.PackageName): PackageCursor;
 
   /**
    * Publish a release
