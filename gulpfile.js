5b0d3015f64c4283eaf5e2aefc0a992b15d5b425
g. nicholas d'andrea <nick@gnidan.org>
2019-02-27 15:11:41 -0500

    Update gulpfile for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -17,47 +17,6 @@ const tsProject = ts.createProject('tsconfig.json', {
   typescript: typescript
 });
 
-gulp.task('compile-source', ['clean-symlinks'], () => {
-  return gulp.src([
-    "src/**/*.ts",
-    "!**/test/*.ts"
-  ])
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel({
-      ignore: ["**/*.d.ts"]
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('compile-test', ['compile-source'], () => {
-  return gulp.src([
-    "test/**/*.ts",
-    "!**/test/*.ts"
-  ])
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel({
-      ignore: ["**/*.d.ts"]
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/test'))
-});
-
-gulp.task('compile', ['compile-source']); // temporariily skip building tests
-
-gulp.task('declarations', ['compile'], function() {
-  dts.bundle({
-    name: "ethpm",
-    main: "dist/ethpm.d.ts",
-    baseDir: "dist",
-    out: "main.d.ts"
-  });
-});
-
 gulp.task('clean-symlinks', () => (
   vfs.src([
     'dist/package.json',
@@ -70,6 +29,47 @@ gulp.task('clean-symlinks', () => (
 ));
 
 
+gulp.task('compile-source', gulp.series(['clean-symlinks'], () => {
+  return gulp.src([
+    "src/**/*.ts",
+    "!**/test/*.ts"
+  ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .pipe(babel({
+      ignore: ["**/*.d.ts"]
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+}));
+
+gulp.task('compile-test', gulp.series(['compile-source'], () => {
+  return gulp.src([
+    "test/**/*.ts",
+    "!**/test/*.ts"
+  ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .pipe(babel({
+      ignore: ["**/*.d.ts"]
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/test'))
+}));
+
+gulp.task('compile', gulp.series(['compile-source']));
+
+gulp.task('declarations', gulp.series(['compile'], async () => (
+  dts.bundle({
+    name: "ethpm",
+    main: "dist/ethpm.d.ts",
+    baseDir: "dist",
+    out: "main.d.ts"
+  })
+)));
+
 gulp.task('copy-readme', () => (
   vfs.src([
     'README.md'
@@ -77,12 +77,12 @@ gulp.task('copy-readme', () => (
     .pipe(vfs.dest('dist'))
 ));
 
-gulp.task('copy-symlinks', ['copy-readme'], () => (
+gulp.task('copy-symlinks', gulp.series(['copy-readme'], () => (
   vfs.src([
     'package.json',
   ], { resolveSymlinks: false })
     .pipe(vfs.symlink('dist'))
-));
+)));
 
 
 gulp.task('docs', () => {
@@ -98,14 +98,14 @@ gulp.task('docs', () => {
     }));
 });
 
-gulp.task('build', ['declarations', 'copy-symlinks']);
+gulp.task('build', gulp.series(['declarations', 'copy-symlinks']));
 
-gulp.task('watch', ['build'], () => {
+gulp.task('watch', gulp.series(['build'], () => {
   return gulp.watch([
     'src/**/*.ts',
     'test/**/*.ts',
     'types/**/*.ts',
   ], ['build'])
-});
+}));
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series(['build']));
